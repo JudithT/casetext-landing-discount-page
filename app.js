385a3407bd11096fdb8bbc3cce0b06cc5feed26a
Judith <judith@Christophers-MacBook-Pro.local>
2019-03-07 18:03:22 -0800

    update

--- app.js
+++ app.js
@@ -1,10 +1,121 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const stripe = require("stripe")("sk_test_wXMJVckQKo8MTy0cIVJ5d0tl");
+const products = [{name: "solo"}, {name: "firm"}, {name: "big firm"}]
 
 // Set up the express app
 const app = express();
 
 
 app.use(express.static(`${__dirname}`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/api/products/add', async (req, res) => {
+  try{
+    const product = await stripe.products.create({
+      name: req.body.name,
+      type: 'service',
+    })
+    return res.send({
+      success: true,
+      product
+    })
+  }catch(e){
+    return res.status(500).send({
+      error: e.message
+    })
+  } 
+})
+
+app.post('/api/products/:id/plan', async (req, res) => {
+  try{
+    const plan = await stripe.plans.create({
+      product: req.params.id,
+      nickname: req.body.name,
+      currency: 'usd',
+      interval: 'year',
+      amount: req.body.amount,
+    });
+
+    return res.send({
+      success: true,
+      plan
+    })
+  }catch(e){
+    return res.status(500).send({
+      error: e.message
+    })
+  } 
+})
+
+app.get('/api/products/:id/plans', async (req, res) => {
+  try{
+    const plans = await stripe.plans.list({
+      product: req.params.id
+    });
+
+    return res.send({
+      success: true,
+      plans: plans.data
+    })
+  }catch(e){
+    return res.status(500).send({
+      error: e.message
+    })
+  } 
+})
+
+app.post('/api/coupon', async (req, res) => {
+  try{
+    const coupon = await stripe.coupons.create({
+      duration: req.body.duration,
+      id: req.body.code,
+      percent_off: req.body.percent_off,
+    });
+
+    return res.send({
+      success: true,
+      coupon
+    })
+  }catch(e){
+    return res.status(500).send({
+      error: e.message
+    })
+  } 
+})
+
+app.get('/api/coupon', async (req, res) => {
+  try{
+    const coupons = await stripe.coupons.list();
+
+    return res.send({
+      success: true,
+      coupons: coupons.data
+    })
+  }catch(e){
+    return res.status(500).send({
+      error: e.message
+    })
+  } 
+})
+
+app.post('/api/subscribe', async (req, res) => {
+  try{
+   await stripe.subscriptions.create({
+      customer: 'cus_EdiG3cquvZVOZZ',
+      items: [{plan: req.body.plan}],
+      coupon: req.body.coupon,
+    });
+    return res.send({
+      success: true,
+    })
+  }catch(e){
+    return res.status(500).send({
+      error: e.message
+    })
+  }
+})
 
 app.all('*', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,16 @@
 let price = 0
 let discount = 0
+let pathname = window.location.pathname
+let pathnames = pathname.split("/")
+let coupons
+let coupon = pathnames[pathnames.length - 1]
+let plan
+
+fetch('/api/coupon')
+.then((res) => res.json())
+.then((data) => {
+  coupons = data.coupons
+})
 
 const promoCodes = { "ct15": 0.15, "ct25": 0.25, "ct30": 0.3 }
 
@@ -11,6 +22,15 @@ const totalPrice = document.getElementById("totalPrice");
 const promoCode = document.getElementById("promoCode");
 
 const updatePrice = () => {
+    let matchCode;
+    coupons.forEach((code) => {
+      if(code.id === coupon){
+        matchCode = code;
+      }
+    }) 
+    if(matchCode){
+      discount = (matchCode.percent_off/100) * price
+    }
     const totalprice = document.createTextNode(`Total Price: ${(price - discount) * 12}`);
     const discountText = document.createTextNode(`Discounts: ${discount * 12}`);
     if(totalPrice.firstChild){
@@ -21,38 +41,59 @@ const updatePrice = () => {
     discountObj.appendChild(discountText)
     totalPrice.appendChild(totalprice);
 }
+
+fetch('/api/products/prod_EepaojYpVep2Ay/plans')
+.then((res) => res.json())
+.then((data) => {
+})
+
 solo.addEventListener("click", () => {
     price = 65;
+    plan = "plan_Eepka9EdjmNRVE"
     updatePrice()
 })
 
 firm.addEventListener("click", () => {
     price = 600;
+    plan = "plan_EeqIuoK4b3v1NR"
     updatePrice()
 })
 
 bigFirm.addEventListener("click", () => {
     price = 900;
+    plan = "plan_EeqKOEwLqjcP1x"
     updatePrice()
 })
 
-promoCode.addEventListener("input", () => {
-    const code = promoCode.value;
-    if(Object.keys(promoCodes).includes(code)){
-        discount = promoCodes[code] * price;
-    }else {
-        discount = 0
-    }
-    updatePrice()
-})
+// promoCode.addEventListener("input", () => {
+//     const code = promoCode.value;
+//     if(Object.keys(promoCodes).includes(code)){
+//         discount = promoCodes[code] * price;
+//     }else {
+//         discount = 0
+//     }
+//     updatePrice()
+// })
 
 var handler = StripeCheckout.configure({
     key: 'pk_test_vl3uMYsGl2xFsIRTKGFeB7Gb',
     image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
     locale: 'auto',
     token: function(token) {
-      // You can access the token ID with `token.id`.
-      // Get the token ID to your server-side code for use.
+      fetch("/api/subscribe", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          plan,
+          coupon
+        })
+    })
+    .then(response => response.json())
+    .then(() => {
+
+    });
     }
   });
   
